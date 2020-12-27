2ee5048797922ede602bafd456f2dba7140d3710
Alejaldo <statfluct@yahoo.com>
2020-12-27 12:37:35 +0500

    Correcto routes: delete independent comments paths and add post_comments_path and images_comments_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :images
-  resources :comments
-  resources :posts
+  resources :images do
+    resources :comments, only: [:create]
+  end
+  
+  resources :posts do
+    resources :comments, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
